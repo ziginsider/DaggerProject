5a2d54018f83112601c63c07e4f95dd1c7b5444d
ziginsider <ziginsider@gmail.com>
2018-06-02 10:01:55 +0300

    add RandomUsers

---
+++ app/src/main/java/io/github/ziginsider/daggerproject/model/RandomUsers.kt
@@ -0,0 +1,6 @@
+package io.github.ziginsider.daggerproject.model
+
+import com.google.gson.annotations.SerializedName
+
+class RandomUsers(@SerializedName("results") val result: List<Results> = null,
+                  @SerializedName("info") val info: Info)
