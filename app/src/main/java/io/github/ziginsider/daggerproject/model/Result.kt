93c25dc44cdfa1d358bbb31144950c5c41c2db2c
ziginsider <ziginsider@gmail.com>
2018-06-02 17:14:18 +0300

    add Result

---
+++ app/src/main/java/io/github/ziginsider/daggerproject/model/Result.kt
@@ -0,0 +1,17 @@
+package io.github.ziginsider.daggerproject.model
+
+import android.graphics.Picture
+import com.google.gson.annotations.SerializedName
+
+class Result(@SerializedName("gender") val gender: String,
+             @SerializedName("name") val name: Name,
+             @SerializedName("location") val location: Location,
+             @SerializedName("email") val email: String,
+             @SerializedName("login") val login: Login,
+             @SerializedName("dob") val dob: String,
+             @SerializedName("registered") val registered: String,
+             @SerializedName("phone") val phone: String,
+             @SerializedName("cell") val cell: String,
+             @SerializedName("id") val id: Id,
+             @SerializedName("picture") val picture: Picture,
+             @SerializedName("nat") val nat: String)
