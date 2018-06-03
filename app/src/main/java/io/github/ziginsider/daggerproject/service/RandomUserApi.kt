fbf93efeb49330a479926f645031d65b2d61e3a1
ziginsider <ziginsider@gmail.com>
2018-06-03 14:57:28 +0300

    add list_item layout

--- app/src/main/java/io/github/ziginsider/daggerproject/service/RandomUserApi.kt
+++ app/src/main/java/io/github/ziginsider/daggerproject/service/RandomUserApi.kt
@@ -1,5 +1,6 @@
 package io.github.ziginsider.daggerproject.service
 
+import io.github.ziginsider.daggerproject.model.RandomUsers
 import retrofit2.Call
 import retrofit2.http.GET
 import retrofit2.http.Query
