0eab2de7421330bd486151dfc3b6fae757f35a06
ziginsider <ziginsider@gmail.com>
2018-06-03 16:27:02 +0300

    add MainActivity::initViews() to init recyclerView

--- app/src/main/java/io/github/ziginsider/daggerproject/MainActivity.kt
+++ app/src/main/java/io/github/ziginsider/daggerproject/MainActivity.kt
@@ -2,11 +2,27 @@ package io.github.ziginsider.daggerproject
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.widget.LinearLayoutManager
+import io.github.ziginsider.daggerproject.adapter.RecyclerViewAdapter
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    private var recyclerAdapter: RecyclerViewAdapter? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        initViews()
+    }
+
+    private fun initViews() {
+        recyclerAdapter = RecyclerViewAdapter(R.layout.list_item,
+                { toast("I'm ${it.name.first} ${it.name.last}") })
+        with(recyclerView) {
+            layoutManager = LinearLayoutManager(this@MainActivity)
+            setHasFixedSize(true)
+            adapter = recyclerAdapter
+        }
     }
 }
