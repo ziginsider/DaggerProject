1eb88d64b844fe23dd460b4baf12dd9712258043
ziginsider <ziginsider@gmail.com>
2018-06-03 15:09:15 +0300

    add RecyclerViewAdapter

---
+++ app/src/main/java/io/github/ziginsider/daggerproject/adapter/RecyclerViewAdapter.kt
@@ -0,0 +1,42 @@
+package io.github.ziginsider.daggerproject.adapter
+
+import android.support.v7.recyclerview.extensions.ListAdapter
+import android.view.ViewGroup
+import io.github.ziginsider.daggerproject.model.Result
+
+/**
+ * Adapter for list of photos [Photo]
+ *
+ * Uses [PhotoDiffCallback] for renew list of items
+ *
+ * @author Alex Kisel
+ * @since 2018-05-15
+ */
+class RecyclerViewAdapter(private val layoutResId: Int, private val clickListener: (Result) -> Unit)
+    : ListAdapter<Result, RecyclerViewAdapter.ViewHolder>(PhotoDiffCallback()) {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val view = parent inflate layoutResId
+        return ViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        holder.bind(getItem(position), clickListener)
+    }
+
+    class ViewHolder(override val containerView: View?)
+        : RecyclerView.ViewHolder(containerView), LayoutContainer {
+
+        fun bind(photo: Photo, clickListener: (Photo) -> Unit) {
+            with(photo) {
+                titlePhoto.text = title
+                if (urlSmall != null) {
+                    ImageLoader.displayImage(imagePhoto, urlSmall)
+                } else if (urlOriginal != null) {
+                    ImageLoader.displayImage(imagePhoto, urlOriginal)
+                }
+                itemView.setOnClickListener { clickListener(this) }
+            }
+        }
+    }
+}
