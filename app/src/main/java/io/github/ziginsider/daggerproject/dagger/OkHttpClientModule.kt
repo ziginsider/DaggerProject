7205a1535214364e778aea2a7f165ca4c26358cf
ziginsider <ziginsider@gmail.com>
2018-06-04 12:44:13 +0300

    add dagger module OkHttpClientModule

---
+++ app/src/main/java/io/github/ziginsider/daggerproject/dagger/OkHttpClientModule.kt
@@ -0,0 +1,36 @@
+package io.github.ziginsider.daggerproject.dagger
+
+import android.content.Context
+import dagger.Module
+import dagger.Provides
+import okhttp3.Cache
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import timber.log.Timber
+import java.io.File
+
+@Module
+class OkHttpClientModule {
+
+    @Provides
+    fun okHttpClient(cache: Cache, httpLoggingInterceptor: HttpLoggingInterceptor) = OkHttpClient()
+            .newBuilder()
+            .cache(cache)
+            .addInterceptor(httpLoggingInterceptor)
+            .build()
+
+    @Provides
+    fun cache(cacheFile: File) = Cache(cacheFile, 10 * 1000 * 1000)
+
+    @Provides
+    fun file(context: Context) = File(context.cacheDir, "HttpCache").mkdirs()
+
+    @Provides
+    fun httpLoggingInterceptor(): HttpLoggingInterceptor {
+        val httpLoggingInterceptor = HttpLoggingInterceptor(HttpLoggingInterceptor.Logger { msg ->
+            Timber.i(msg)
+        })
+        httpLoggingInterceptor.level = HttpLoggingInterceptor.Level.BODY
+        return httpLoggingInterceptor
+    }
+}
