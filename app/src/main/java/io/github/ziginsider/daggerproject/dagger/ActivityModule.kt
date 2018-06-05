14706e90bf50774e5ae1ea248b85d58347ed91f4
ziginsider <ziginsider@gmail.com>
2018-06-05 11:06:42 +0300

    add @Named to context modules

--- app/src/main/java/io/github/ziginsider/daggerproject/dagger/ActivityModule.kt
+++ app/src/main/java/io/github/ziginsider/daggerproject/dagger/ActivityModule.kt
@@ -3,10 +3,12 @@ package io.github.ziginsider.daggerproject.dagger
 import android.content.Context
 import dagger.Module
 import dagger.Provides
+import javax.inject.Named
 
 @Module
 class ActivityModule(val context: Context) {
 
+    @Named("activity_context")
     @RandomUserApplicationScope
     @Provides
     fun context() = context
--- app/src/main/java/io/github/ziginsider/daggerproject/dagger/ContextModule.kt
+++ app/src/main/java/io/github/ziginsider/daggerproject/dagger/ContextModule.kt
@@ -3,10 +3,13 @@ package io.github.ziginsider.daggerproject.dagger
 import android.content.Context
 import dagger.Module
 import dagger.Provides
+import javax.inject.Named
 
 @Module
 class ContextModule(val context: Context) {
 
+    @Named("application_context")
+    @RandomUserApplicationScope
     @Provides
     fun context(): Context = context.applicationContext
 }
