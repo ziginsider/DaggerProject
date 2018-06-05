4e3ce2b9c46be8d2dd4dbaeed8397e56ecca014d
ziginsider <ziginsider@gmail.com>
2018-06-05 10:48:28 +0300

    add scope annotation RandomUserApplicationScope

---
+++ app/src/main/java/io/github/ziginsider/daggerproject/dagger/RandomUserApplicationScope.kt
@@ -0,0 +1,7 @@
+package io.github.ziginsider.daggerproject.dagger
+
+import javax.inject.Scope
+
+@Scope
+@kotlin.annotation.Retention(AnnotationRetention.BINARY)
+annotation class RandomUserApplicationScope
